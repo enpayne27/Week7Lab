414a401a364511d3d8376461493f4feec42f93ad
Ashley Coleman <me@ashleycoleman.me>
2018-09-30 20:27:41 -0500

    Week 7 Lab starting files

---
+++ core.c
@@ -0,0 +1,56 @@
+//
+//  core.c
+//  Week 7 
+//
+//  Created by Ashley Coleman on 7/5/18.
+//  Copyright © 2018 Ashley Coleman. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "core.h"
+
+// This function should initialize the 
+// LinkedList to be a node holing the int "value"
+// Make sure to also set the lists "type"
+// return FALSE if error, TRUE otherwise
+bool create_integer_node(LinkedList * list, int value) {
+    return FALSE;
+}
+
+// This function should initialize the 
+// LinkedList to be a node holing the char "value"
+// Make sure to also set the lists "type"
+// return FALSE if error, TRUE otherwise
+bool create_character_node(LinkedList * list, char value) {
+    return FALSE;
+}
+
+// This function should initialize the 
+// LinkedList to be a node holing the float "value"
+// Make sure to also set the lists "type"
+// return FALSE if error, TRUE otherwise
+bool create_float_node(LinkedList * list, float value) {
+    return FALSE;
+}
+
+// This function should sum *only* the nodes
+// which hold an integer. All other nodes should
+// be ignored.
+// return 0 if no list is provided
+int sum_integer_nodes(LinkedList * list) {
+    return 0;
+}
+
+// This function should sum *only* the nodes
+// which hold a float. All other nodes should
+// be ignored.
+// return 0.0 if no list is provided
+float sum_float_nodes(LinkedList * list) {
+    return 0.0;
+}
+
+// Nothing needs to be done in core_main
+int core_main(int argc, const char * argv[]) {
+    return 0;
+}
---
+++ types.h
@@ -0,0 +1,23 @@
+//
+//  types.h
+//  Week 7 
+//
+//  Created by Ashley Coleman on 7/5/18.
+//  Copyright © 2018 Ashley Coleman. All rights reserved.
+//
+
+#ifndef types_h
+#define types_h
+
+typedef enum { FALSE, TRUE } bool;
+
+// Define a typedef'd struct called "LinkedList" with the following variables 
+// An enum variable called "type" with the values "Integer", "Character", "Float"
+// A union variable called "value" with the following variables
+//     an int called "i"
+//     a char called "c"
+//     a float called "f"
+// A struct LinkedList pointer called next
+
+
+#endif /* types_h */
